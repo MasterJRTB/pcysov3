c0de3680adfe0f4529bd9f2ab37a5fb809d04df3
MasterJRTB <34519445+MasterJRTB@users.noreply.github.com>
2018-07-31 17:03:03 -0500

    Pagina

---
+++ pcyso2018/PCySO2018/PCySO2018/ViewModels/PaginaPrincipalViewModel.cs
@@ -0,0 +1,63 @@
+﻿namespace PCySO2018.ViewModels
+{
+    using System.Collections.ObjectModel;
+    using Xamarin.Forms;
+
+
+    public class PaginaPrincipalViewModel : BaseViewModel
+    {
+
+        #region Atributos
+        private ObservableCollection<PaginaPrincipalViewModel> paginaPrincipal;
+        #endregion
+
+        #region Propiedades
+        public ObservableCollection<PaginaPrincipalViewModel> PaginaPrincipal
+        {
+            get { return this.paginaPrincipal; }
+            set { SetValue(ref this.paginaPrincipal, value); }
+        }
+        #endregion
+
+        #region Constructores
+        public PaginaPrincipalViewModel()
+        {
+            this.apiService = new ApiService();
+            this.LoadPaginaPrincipal();
+        }
+        #endregion
+
+        #region Metodos
+        private async void LoadPaginaPrincipal()
+        {
+            var connection = await this.apiService.CheckConnection();
+            if (!connection.IsSuccess)
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    connection.Message,
+                    "Aceptar");
+                await Application.Current.MainPage.Navigation.PopAsync();
+                return;
+            }
+
+            
+            //if (!response.IsSuccess)
+            //{
+            //    await Application.Current.MainPage.DisplayAlert(
+            //        "Error",
+            //        response.Message,
+            //        "Aceptar");
+            //    await Application.Current.MainPage.Navigation.PopAsync();
+            //    return;
+            //}
+            //var response = await this.apiService.GetList<PaginaPrincipal>("", "", "");
+            //throw new NotImplementedException();
+        }
+        #endregion
+
+        #region Servicios
+        private ApiService apiService;
+        #endregion
+    }
+}
