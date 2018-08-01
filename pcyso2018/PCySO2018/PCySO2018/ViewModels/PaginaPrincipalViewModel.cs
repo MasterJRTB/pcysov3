62e1ae134c30365def62a85232ca48041f19c705
MasterJRTB <34519445+MasterJRTB@users.noreply.github.com>
2018-08-01 14:27:15 -0500

    pa

--- pcyso2018/PCySO2018/PCySO2018/ViewModels/PaginaPrincipalViewModel.cs
+++ pcyso2018/PCySO2018/PCySO2018/ViewModels/PaginaPrincipalViewModel.cs
@@ -1,64 +1,115 @@
 ﻿namespace PCySO2018.ViewModels
 {
-    using PCySO2018.Services;
-    using System.Collections.ObjectModel;
-    using Xamarin.Forms;
-
+    using Java.Lang;
+    using System.Collections;
+    using System.Collections.Generic;
+    //using static Android.Util.EventLogTags;
 
     public class PaginaPrincipalViewModel : BaseViewModel
     {
 
         #region Atributos
-        private ObservableCollection<PaginaPrincipalViewModel> paginaPrincipal;
+        ////private ObservableCollection<PaginaPrincipalViewModel> paginaPrincipal
+        public string Number { get; internal set; }
+        public string Nombre { get; internal set; }
+        public string Description { get; internal set; }
+        public string Descripcion{ get; internal set;}
+        public IEnumerable _elementos { get; internal set; }
         #endregion
 
         #region Propiedades
-        public ObservableCollection<PaginaPrincipalViewModel> PaginaPrincipal
-        {
-            get { return this.paginaPrincipal; }
-            set { SetValue(ref this.paginaPrincipal, value); }
-        }
+        //public ObservableCollection<PaginaPrincipalViewModel> PaginaPrincipal
+        //{
+        //    get { return this.paginaPrincipal; }
+        //    set { SetValue(ref this.paginaPrincipal, value); }
+        //}
         #endregion
 
         #region Constructores
-        public PaginaPrincipalViewModel()
-        {
-            this.apiService = new ApiService();
-            this.LoadPaginaPrincipal();
-        }
+        //public PaginaPrincipalViewModel()
+        //{
+        //    this.apiService = new ApiService();
+        //    this.LoadPaginaPrincipal();
+        //}
         #endregion
 
         #region Metodos
-        private async void LoadPaginaPrincipal()
-        {
-            var connection = await this.apiService.CheckConnection();
-            if (!connection.IsSuccess)
-            {
-                await Application.Current.MainPage.DisplayAlert(
-                    "Error",
-                    connection.Message,
-                    "Aceptar");
-                await Application.Current.MainPage.Navigation.PopAsync();
-                return;
-            }
+        //private async void LoadPaginaPrincipal()
+        //{
+        //    var connection = await this.apiService.CheckConnection();
+        //    if (!connection.IsSuccess)
+        //    {
+        //        await Application.Current.MainPage.DisplayAlert(
+        //            "Error",
+        //            connection.Message,
+        //            "Aceptar");
+        //        await Application.Current.MainPage.Navigation.PopAsync();
+        //        return;
+        //    }
 
-            
-            //if (!response.IsSuccess)
-            //{
-            //    await Application.Current.MainPage.DisplayAlert(
-            //        "Error",
-            //        response.Message,
-            //        "Aceptar");
-            //    await Application.Current.MainPage.Navigation.PopAsync();
-            //    return;
-            //}
-            //var response = await this.apiService.GetList<PaginaPrincipal>("", "", "");
-            //throw new NotImplementedException();
-        }
+
+        //if (!response.IsSuccess)
+        //{
+        //    await Application.Current.MainPage.DisplayAlert(
+        //        "Error",
+        //        response.Message,
+        //        "Aceptar");
+        //    await Application.Current.MainPage.Navigation.PopAsync();
+        //    return;
+        //}
+        //var response = await this.apiService.GetList<PaginaPrincipal>("", "", "");
+        //throw new NotImplementedException();
+        //}
         #endregion
 
         #region Servicios
-        private ApiService apiService;
+        //private ApiService apiService;
         #endregion
+    }
+
+    public class ListaElmentos
+    {
+        public List<PaginaPrincipalViewModel> _elementos
+        {
+            get;
+            set;
+        }
+        private string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public static object ItemsSource { get; internal set; }
+
+        public ListaElmentos()
+        {
+            _elementos = new List<PaginaPrincipalViewModel>();
+            LoadElements();
+        }
+
+        public void LoadElements()
+        {
+            _elementos.Add(new PaginaPrincipalViewModel
+            {
+                Nombre = "Recursos Humanos",
+                Descripcion = "En este módulo se lleva el control de: Trabajador, Contratista, Proveedor"
+            });
+
+            _elementos.Add(new PaginaPrincipalViewModel
+            {
+                Nombre = "Recursos Materiales",
+                Descripcion = "En este módulo se lleva el control de: Extintor, Bitacora Extintor, Inmueble"
+            });
+
+            _elementos.Add(new PaginaPrincipalViewModel
+            {
+                Nombre = "Unidad Médica",
+                Descripcion = "En este módulo se lleva el control de: Antecedentes, Atención, Incapacidad del Trabajador"
+            });
+
+            _elementos.Add(new PaginaPrincipalViewModel
+            {
+                Nombre = "Ayuda",
+                Descripcion = "Este módulo sirve como referencia básica"
+            });
+
+        }
     }
 }
--- pcyso2018/PCySO2018/PCySO2018/Views/PaginaPrincipalPage.xaml.cs
+++ pcyso2018/PCySO2018/PCySO2018/Views/PaginaPrincipalPage.xaml.cs
@@ -1,20 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
+using PCySO2018.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace PCySO2018.Views
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class PaginaPrincipalPage : ContentPage
 	{
 		public PaginaPrincipalPage ()
 		{
 			InitializeComponent ();
 		}
-	}
+
+        public PaginaPrincipalViewModel PaginaPrincipalViewModel { get; private set; }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            PaginaPrincipalViewModel = new PaginaPrincipalViewModel();
+            General.ItemsSource = PaginaPrincipalViewModel._elementos;
+
+            
+        }
+    }
 }
