bc26f62a706fb03bc75218fce5ef80798244f879
MasterJRTB <34519445+MasterJRTB@users.noreply.github.com>
2018-08-03 17:08:23 -0500

    t

--- pcyso2018/PCySO2018/PCySO2018.Android/Resources/Resource.designer.cs
+++ pcyso2018/PCySO2018/PCySO2018.Android/Resources/Resource.designer.cs
@@ -2344,26 +2344,26 @@ namespace PCySO2018.Droid
 			// aapt resource value: 0x7f020053
 			public const int avd_hide_password = 2130837587;
 			
-			// aapt resource value: 0x7f020138
-			public const int avd_hide_password_1 = 2130837816;
+			// aapt resource value: 0x7f02013d
+			public const int avd_hide_password_1 = 2130837821;
 			
-			// aapt resource value: 0x7f020139
-			public const int avd_hide_password_2 = 2130837817;
+			// aapt resource value: 0x7f02013e
+			public const int avd_hide_password_2 = 2130837822;
 			
-			// aapt resource value: 0x7f02013a
-			public const int avd_hide_password_3 = 2130837818;
+			// aapt resource value: 0x7f02013f
+			public const int avd_hide_password_3 = 2130837823;
 			
 			// aapt resource value: 0x7f020054
 			public const int avd_show_password = 2130837588;
 			
-			// aapt resource value: 0x7f02013b
-			public const int avd_show_password_1 = 2130837819;
+			// aapt resource value: 0x7f020140
+			public const int avd_show_password_1 = 2130837824;
 			
-			// aapt resource value: 0x7f02013c
-			public const int avd_show_password_2 = 2130837820;
+			// aapt resource value: 0x7f020141
+			public const int avd_show_password_2 = 2130837825;
 			
-			// aapt resource value: 0x7f02013d
-			public const int avd_show_password_3 = 2130837821;
+			// aapt resource value: 0x7f020142
+			public const int avd_show_password_3 = 2130837826;
 			
 			// aapt resource value: 0x7f020055
 			public const int checkronmini = 2130837589;
@@ -3019,11 +3019,11 @@ namespace PCySO2018.Droid
 			// aapt resource value: 0x7f02012e
 			public const int notification_icon_background = 2130837806;
 			
-			// aapt resource value: 0x7f020136
-			public const int notification_template_icon_bg = 2130837814;
+			// aapt resource value: 0x7f02013b
+			public const int notification_template_icon_bg = 2130837819;
 			
-			// aapt resource value: 0x7f020137
-			public const int notification_template_icon_low_bg = 2130837815;
+			// aapt resource value: 0x7f02013c
+			public const int notification_template_icon_low_bg = 2130837820;
 			
 			// aapt resource value: 0x7f02012f
 			public const int notification_tile_bg = 2130837807;
@@ -3038,13 +3038,28 @@ namespace PCySO2018.Droid
 			public const int sesion = 2130837810;
 			
 			// aapt resource value: 0x7f020133
-			public const int tooltip_frame_dark = 2130837811;
+			public const int textoayuda = 2130837811;
 			
 			// aapt resource value: 0x7f020134
-			public const int tooltip_frame_light = 2130837812;
+			public const int textopaginaprincipal = 2130837812;
 			
 			// aapt resource value: 0x7f020135
-			public const int twiter_logo2 = 2130837813;
+			public const int textorecursoshumanos = 2130837813;
+			
+			// aapt resource value: 0x7f020136
+			public const int textorecursosmateriales = 2130837814;
+			
+			// aapt resource value: 0x7f020137
+			public const int textounidadmedica = 2130837815;
+			
+			// aapt resource value: 0x7f020138
+			public const int tooltip_frame_dark = 2130837816;
+			
+			// aapt resource value: 0x7f020139
+			public const int tooltip_frame_light = 2130837817;
+			
+			// aapt resource value: 0x7f02013a
+			public const int twiter_logo2 = 2130837818;
 			
 			static Drawable()
 			{
