84a090887ff7ba86295b259f6d257e6eb7611fe6
MasterJRTB <34519445+MasterJRTB@users.noreply.github.com>
2018-08-04 11:59:57 -0500

    c

--- pcyso2018/PCySO2018/PCySO2018/Views/HumanosPage.xaml.cs
+++ pcyso2018/PCySO2018/PCySO2018/Views/HumanosPage.xaml.cs
@@ -15,6 +15,25 @@ namespace PCySO2018.Views
 		public HumanosPage ()
 		{
 			InitializeComponent ();
+            btnTrabajador.Clicked += BtnTrabajador_Clicked;
+            btnContratista.Clicked += BtnContratista_Clicked;
+            btnProveedor.Clicked += BtnProveedor_Clicked;
 		}
-	}
+
+        private void BtnTrabajador_Clicked(object sender, EventArgs e)
+        {
+            //throw new NotImplementedException();
+            ((NavigationPage)this.Parent).PushAsync(new TrabajadorPage());
+        }
+        private void BtnContratista_Clicked(object sender, EventArgs e)
+        {
+            //throw new NotImplementedException();
+            ((NavigationPage)this.Parent).PushAsync(new ContratistaPage());
+        }
+        private void BtnProveedor_Clicked(object sender, EventArgs e)
+        {
+            //throw new NotImplementedException();
+            ((NavigationPage)this.Parent).PushAsync(new ProveedorPage());
+        }
+    }
 }
