0a5f8c123e878635987f81f6b1a155989ec44d12
MasterJRTB <34519445+MasterJRTB@users.noreply.github.com>
2018-08-08 15:01:14 -0500

    t

--- pcyso2018/PCySO2018/PCySO2018/Models/TrabajadorModel.cs
+++ pcyso2018/PCySO2018/PCySO2018/Models/TrabajadorModel.cs
@@ -1,11 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿
 
 namespace PCySO2018.Models
 {
-    public class TrabajadorModel
+    using System;
+    using System.ComponentModel;
+    using SQLite.Net.Attributes;
+    public class TrabajadorModel: INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
         private int idTrabajador;
 
         private string nombreTrabajador;
@@ -36,5 +39,36 @@ namespace PCySO2018.Models
         private string pareTrabajador;
         private string tceTrabajador;
         private string celceTrabajador;
+
+        [PrimaryKey,AutoIncrement]
+        public int IdTrabajador { get => idTrabajador; set => idTrabajador = value; }
+
+        public string NombreTrabajador { get => nombreTrabajador; set => nombreTrabajador = value; }
+        public string TipoTrabajador { get => tipoTrabajador; set => tipoTrabajador = value; }
+        public string CtTrabajador { get => ctTrabajador; set => ctTrabajador = value; }
+        public string DepaTrabajador { get => depaTrabajador; set => depaTrabajador = value; }
+        public string PueTrabajador { get => pueTrabajador; set => pueTrabajador = value; }
+        public string HorTrabajador { get => horTrabajador; set => horTrabajador = value; }
+        public string TelofiTrabajador { get => telofiTrabajador; set => telofiTrabajador = value; }
+        public string ExttelTrabajador { get => exttelTrabajador; set => exttelTrabajador = value; }
+        public string EmailTrabajador { get => emailTrabajador; set => emailTrabajador = value; }
+        public DateTime FaTrabajador { get => faTrabajador; set => faTrabajador = value; }
+        public string NssTrabajador { get => nssTrabajador; set => nssTrabajador = value; }
+        public string NjTrabajador { get => njTrabajador; set => njTrabajador = value; }
+        public string TjTrabajador { get => tjTrabajador; set => tjTrabajador = value; }
+        public string EtjTrabajador { get => etjTrabajador; set => etjTrabajador = value; }
+        public string EmjTrabajador { get => emjTrabajador; set => emjTrabajador = value; }
+        public string CalTrabajador { get => calTrabajador; set => calTrabajador = value; }
+        public string NeTrabajador { get => neTrabajador; set => neTrabajador = value; }
+        public string NiTrabajador { get => niTrabajador; set => niTrabajador = value; }
+        public string ColTrabajador { get => colTrabajador; set => colTrabajador = value; }
+        public string CpTrabajador { get => cpTrabajador; set => cpTrabajador = value; }
+        public string DmTrabajador { get => dmTrabajador; set => dmTrabajador = value; }
+        public string NceTrabajador { get => nceTrabajador; set => nceTrabajador = value; }
+        public string PareTrabajador { get => pareTrabajador; set => pareTrabajador = value; }
+        public string TceTrabajador { get => tceTrabajador; set => tceTrabajador = value; }
+        public string CelceTrabajador { get => celceTrabajador; set => celceTrabajador = value; }
+
+        
     }
 }
