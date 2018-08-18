4272c104c5021334dc19eadd6e03ac960793e009
MasterJRTB <34519445+MasterJRTB@users.noreply.github.com>
2018-08-18 13:27:17 -0500

    t

---
+++ pcyso2018/PCySO2018/PCySO2018/Views/TrabajadorDatosPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using PCySO2018.Models;
+using PCySO2018.ViewModels;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PCySO2018.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TrabajadorDatosPage : ContentPage
+    {
+
+        public TrabajadorDatosPage()
+        {
+            InitializeComponent();
+            this.BindingContext = new TrabajadorDatosViewModel();
+            
+        }
+
+
+    }
+}
--- pcyso2018/PCySO2018/PCySO2018/Views/TrabajadorListadoPage.xaml.cs
+++ pcyso2018/PCySO2018/PCySO2018/Views/TrabajadorListadoPage.xaml.cs
@@ -1,23 +1,35 @@
-﻿
+﻿using PCySO2018.ViewModels;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
 namespace PCySO2018.Views
 {
-    using PCySO2018.Models;
-    using PCySO2018.ViewModels;
-    using System.Collections.ObjectModel;
-    using Xamarin.Forms;
-    using Xamarin.Forms.Xaml;
-    
 
     [XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class TrabajadorListadoPage : ContentPage
     {
-        
+      
+
         public TrabajadorListadoPage()
         {
             InitializeComponent();
+            
             this.BindingContext = new TrabajadorListadoViewModel();
+            //btnSeleccionado.Clicked += BtnSeleccionado_Clicked;
         }
 
         
+        //private void BtnSeleccionado_Clicked(object sender, EventArgs e)
+        //{
+        //    //throw new NotImplementedException();
+        //    ((NavigationPage)this.Parent).PushAsync(new TrabajadorDatosPage());
+        //}
+        //private void SelectTrabajadorCommand()
+        //{
+        //    ((NavigationPage)this.Parent).PushAsync(new TrabajadorDatosPage());
+        //}
     }
+
+   
 }
